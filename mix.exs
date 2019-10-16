6db48bae347bc8d8537c999abad6c96b8c5aa51f
sphaso <sphaso@gmail.com>
2019-10-16 09:10:22 +0200

    Release 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Noether.MixProject do
   def project do
     [
       app: :noether,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
