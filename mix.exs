39ee85e4da8d481da6c15fcb12a27d8d40c13d61
sphaso <sphaso@gmail.com>
2019-11-22 11:39:19 +0100

    bump 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Noether.MixProject do
   def project do
     [
       app: :noether,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
