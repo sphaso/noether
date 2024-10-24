23a1f4896d6143ac5174dbabed3d3b1da6e6ab0a
Giovanni Ornaghi <4958220+sphaso@users.noreply.github.com>
2024-10-24 18:44:12 +0200

    Remove zip_with, version 1 (#39)
    Co-authored-by: sphaso <sphaso@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Noether.MixProject do
   def project do
     [
       app: :noether,
-      version: "0.3.0",
+      version: "1.0.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
